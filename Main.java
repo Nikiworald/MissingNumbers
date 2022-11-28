0a5a7d567df4e3eb89a5f1704e9eb1e7c7d30f27
Nikiworald <50948755+Nikiworald@users.noreply.github.com>
2022-11-28 18:03:48 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int start = 6;
+        int end = 20;
+        int lenght = end - start;
+        int sumArray = 0;
+        int sumNumber = ((start+end)/2)*(end-start+1);
+        int missingNumber=0;
+        ArrayGenerator arrayGenerator = new ArrayGenerator(lenght);
+        int[]array = arrayGenerator.arrayInTheMiddle(start,end);
+        System.out.println(Arrays.toString(array));
+        for (int i = 0;i<lenght-1;i++){
+            sumArray += array[i];
+        }
+        missingNumber = (sumNumber-sumArray)-end;
+        System.out.println(missingNumber);
+    }
+}
